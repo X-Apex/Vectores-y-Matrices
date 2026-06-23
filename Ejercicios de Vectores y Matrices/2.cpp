d31f3d9690b1601dab5922123141d936219806dd
Bell <isabelaccilio@gmail.com>
2026-06-23 06:35:25 -0500

    Update 2.cpp

--- Ejercicios de Vectores y Matrices/2.cpp
+++ Ejercicios de Vectores y Matrices/2.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Ingrese tamaño del vector: ";
+    cout << "Ingrese el tamaño del vector: ";
     cin >> n;
     int v[n], suma = 0;
+    cout << "Ingrese " << n << " elementos:" << endl;
     for(int i=0; i<n; i++){
         cin >> v[i];
         suma += v[i]*v[i];
     }
-    cout << "Suma de cuadrados = " << suma << endl;
+    cout << "La suma de cuadrados es: " << suma << endl;
     return 0;
 }
