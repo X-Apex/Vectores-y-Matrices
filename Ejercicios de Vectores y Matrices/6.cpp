1ba3caa2533a1a25d64cc082251c29ec37faf137
Bell <isabelaccilio@gmail.com>
2026-06-23 06:38:40 -0500

    Update 6.cpp

--- Ejercicios de Vectores y Matrices/6.cpp
+++ Ejercicios de Vectores y Matrices/6.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
     int n;
+    cout << "Ingrese el tamaño del vector: ";
     cin >> n;
     int v[n];
+    cout << "Ingrese " << n << " elementos:" << endl;
     for(int i=0; i<n; i++) cin >> v[i];
     int minimo = v[0];
     for(int i=1; i<n; i++) if(v[i]<minimo) minimo=v[i];
-    cout << "Mínimo = " << minimo << endl;
+    cout << "El mínimo es: " << minimo << endl;
     return 0;
 }
