ea376b4104b7a1b17c0357bf8e6bda582e05d6c2
Bell <isabelaccilio@gmail.com>
2026-06-23 06:40:44 -0500

    Add prompts for matrix dimensions and elements

--- Ejercicios de Vectores y Matrices/9.cpp
+++ Ejercicios de Vectores y Matrices/9.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main() {
     int m,n;
+    cout << "Ingrese filas y columnas de la matriz: ";
     cin >> m >> n;
     int A[m][n];
+    cout << "Ingrese los elementos de la matriz:" << endl;
     for(int i=0;i<m;i++) for(int j=0;j<n;j++) cin>>A[i][j];
     for(int i=0;i<m;i++){
         int suma=0;
         for(int j=0;j<n;j++) suma+=A[i][j];
-        cout<<"Fila "<<i+1<<": "<<suma<<endl;
+        cout<<"Suma fila "<<i+1<<": "<<suma<<endl;
     }
     return 0;
 }
