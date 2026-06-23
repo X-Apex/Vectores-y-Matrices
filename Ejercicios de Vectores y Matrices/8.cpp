f18ccc7c2a42f355bff5bf184a0259d88cd277f6
Bell <isabelaccilio@gmail.com>
2026-06-23 06:40:23 -0500

    Update 8.cpp

--- Ejercicios de Vectores y Matrices/8.cpp
+++ Ejercicios de Vectores y Matrices/8.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main() {
     int m,n;
+    cout << "Ingrese filas y columnas de la matriz: ";
     cin >> m >> n;
     int A[m][n];
+    cout << "Ingrese los elementos de la matriz:" << endl;
     for(int i=0;i<m;i++) for(int j=0;j<n;j++) cin>>A[i][j];
     int menor=A[0][0], mayor=A[0][0];
     for(int i=0;i<m;i++) for(int j=0;j<n;j++){
