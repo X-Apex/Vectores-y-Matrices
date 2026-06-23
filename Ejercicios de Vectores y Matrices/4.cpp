70e326f77490c5a0d03b68baf005edd78aabc4ae
Bell <isabelaccilio@gmail.com>
2026-06-23 06:36:19 -0500

    Refactor vector multiplication program prompts

--- Ejercicios de Vectores y Matrices/4.cpp
+++ Ejercicios de Vectores y Matrices/4.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main() {
     int n, k;
-    cout << "Ingrese tamaño del vector: ";
+    cout << "Ingrese el tamaño del vector: ";
     cin >> n;
-    cout << "Ingrese número k: ";
+    cout << "Ingrese el número a multiplicar: ";
     cin >> k;
     int v[n];
+    cout << "Ingrese " << n << " elementos:" << endl;
     for(int i=0; i<n; i++) cin >> v[i];
     cout << "Vector resultante: ";
     for(int i=0; i<n; i++) cout << v[i]*k << " ";
