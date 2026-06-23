9fabdb5ac88c2cf2cfd3bea178db3440e1149f5a
Bell <isabelaccilio@gmail.com>
2026-06-23 06:35:54 -0500

    Improve user prompts and output formatting

--- Ejercicios de Vectores y Matrices/3.cpp
+++ Ejercicios de Vectores y Matrices/3.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Ingrese tamaño de los vectores: ";
+    cout << "Ingrese el tamaño de los vectores: ";
     cin >> n;
     int v[n], w[n], producto = 0;
+    cout << "Ingrese los elementos del primer vector:" << endl;
     for(int i=0; i<n; i++) cin >> v[i];
+    cout << "Ingrese los elementos del segundo vector:" << endl;
     for(int i=0; i<n; i++) cin >> w[i];
     for(int i=0; i<n; i++) producto += v[i]*w[i];
-    cout << "Producto escalar = " << producto << endl;
+    cout << "El producto escalar es: " << producto << endl;
     return 0;
 }
