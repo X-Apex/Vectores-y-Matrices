52f41960ada607887ab18bcc4cf09b4b2b687605
Bell <isabelaccilio@gmail.com>
2026-06-23 06:34:43 -0500

    Update 10.cpp

--- Ejercicios de Vectores y Matrices/10.cpp
+++ Ejercicios de Vectores y Matrices/10.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main() {
     int m,n;
+    cout << "Ingrese filas y columnas de la matriz: ";
     cin >> m >> n;
     int A[m][n];
+    cout << "Ingrese los elementos de la matriz:" << endl;
     for(int i=0;i<m;i++) for(int j=0;j<n;j++) cin>>A[i][j];
     for(int j=0;j<n;j++){
         int suma=0;
         for(int i=0;i<m;i++) suma+=A[i][j];
-        cout<<"Columna "<<j+1<<": "<<suma<<endl;
+        cout<<"Suma columna "<<j+1<<": "<<suma<<endl;
     }
     return 0;
 }
